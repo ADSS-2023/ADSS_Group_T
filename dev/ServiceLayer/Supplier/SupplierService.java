26956ea4d520ab59d5318032a8826ad86bd0a394
omerguz <guzo@post.bgu.ac.il>
2023-03-28 22:56:53 +0300

    changes to SupplierService.

--- dev/ServiceLayer/Supplier/SupplierService.java
+++ dev/ServiceLayer/Supplier/SupplierService.java
@@ -1,5 +1,12 @@
 package ServiceLayer.Supplier;
 
+import BusinessLayer.Supplier.SupplierProductBusiness;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class SupplierService {
     private SupplierController sc;
 
@@ -7,17 +14,23 @@ public class SupplierService {
         this.sc = sc;
     }
     public List<String> getProducts(int vendorNum){
-
+        HashMap<Integer, SupplierProductBusiness> productMap =  sc.getProducts(vendorNum);
+        List<String> products = new ArrayList<String>();
+        for (Map.Entry<Integer, SupplierProductBusiness> entry : productMap.entrySet()) {
+            products.add(entry.getKey() + entry.getValue().toString());
+        }
+        return products;
     }
 
-    public List<String> addProduct(int vendorNum...){
-
+    public void addProduct(int supplierNum, String productName, String manufacturer, int price, int maxAmount){
+        sc.addProduct(supplierNum, productName, manufacturer, price, maxAmount);
     }
 
-    public List<String> editProduct(int vendorNum...){
-
+    public void editProduct(int supplierNum, String ProductName, String manufacturer, int price, int maxAmount){
+        sc.editProduct(supplierNum, productName, manufacturer, price, maxAmount);
     }
 
-    public List<String> deleteProduct(int vendorNum...)
-}
+    public void deleteProduct(int supplierNum, String ProductName, String manufacturer, int price, int maxAmount){
+        sc.deleteProduct(supplierNum, productName, manufacturer, price, maxAmount);
+    }
 }
