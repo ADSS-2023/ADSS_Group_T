dd98a5259f9c4ac3b7054ffe1a1504d15f47d26f
Yagilbr <yagilbr@bgu.post.ac.il>
2023-03-28 23:29:36 +0300

    first order business changes

---
+++ dev/BusinessLayer/Supplier/OrderBusiness.java
@@ -0,0 +1,36 @@
+package BusinessLayer.Supplier;
+
+import java.util.Date;
+import java.util.HashMap;
+
+
+public class OrderBusiness {
+
+    private int orderNum;
+    private String supplierName;
+    private Date orderDate;
+    private String supplierAddress;
+    private String destinationAddress;
+    private int supplierNum;
+    private int contactName;
+    private int contactNumber;
+    private HashMap<Integer,Integer> products ;
+
+    public OrderBusiness(int orderNum, String supplierName, Date orderDate,
+                         String supplierAddress, String destinationAddress
+            ,int supplierNum, int contactName, int contactNumber,
+                         HashMap<Integer, Integer> products) {
+        this.orderNum = orderNum;
+        this.supplierName = supplierName;
+        this.orderDate = orderDate;
+        this.supplierAddress = supplierAddress;
+        this.destinationAddress = destinationAddress;
+        this.supplierNum = supplierNum;
+        this.contactName = contactName;
+        this.contactNumber = contactNumber;
+        this.products = products;
+    }
+
+
+
+}
