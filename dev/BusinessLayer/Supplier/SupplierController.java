e46589e26dbb87083aaae8c8150044879b26ec88
omerguz <guzo@post.bgu.ac.il>
2023-03-29 00:00:02 +0300

    I added function for add, delete and edit supplier.

--- dev/BusinessLayer/Supplier/SupplierController.java
+++ dev/BusinessLayer/Supplier/SupplierController.java
@@ -1,6 +1,8 @@
 package BusinessLayer.Supplier;
 
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SupplierController {
     HashMap<Integer, SupplierBusiness> suppliers;
@@ -19,5 +21,17 @@ public class SupplierController {
 
     public void deleteProduct(int supplierNum, String productName, String manufacturer, int price, int maxAmount){
         suppliers.get(supplierNum).deleteProduct(productName, manufacturer, price, maxAmount);
+    }
+
+    public void addSupplier(String name, String address, int supplierNum, int bankAccountNum, Map<String, Integer> contacts, List<String> constDeliveryDays, boolean selfDelivery, Map<Integer, SupplierProductBusiness> products){
+        suppliers.put(supplierNum, new SupplierBusiness(name,address,supplierNum, bankAccountNum, contacts, constDeliveryDays, selfDelivery, products));
+    }
+
+    public void deleteSupplier(int supplierNum){
+        suppliers.remove(supplierNum);
+    }
+
+    public void editSupplier(addSupplier(String name, String address, int supplierNum,int bankAccountNum, Map<String, Integer> contacts, List<String> constDeliveryDays, boolean selfDelivery, Map<Integer, SupplierProductBusiness> products){
+        suppliers.get(supplierNum).editSupplier(name,address, bankAccountNum, contacts, constDeliveryDays, selfDelivery, products);
     }
 }
