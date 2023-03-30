91c243b7aa0e15767b62f378452503b32dadd1ee
Noam Shurki <shurkin@post.bgu.ac.il>
2023-03-30 11:56:52 +0300

    empty constructor added

--- dev/BusinessLayer/Transport/LogisticsCenter.java
+++ dev/BusinessLayer/Transport/LogisticsCenter.java
@@ -6,25 +6,34 @@ import BusinessLayer.Transport.Driver.CoolingLevel;
 
 public class LogisticsCenter {
 
-private  HashMap<Integer,Truck> trucks;
-private  HashMap<Integer,Delivery> deliveries;
-private  HashMap<Product,Integer> products;
-private  HashMap<Integer,Driver> drivers;
-private  HashMap<LocalDate,ArrayList<Truck>> date2trucks;
-private  HashMap<LocalDate,ArrayList<Driver>> date2drivers;
-private  HashMap<LocalDate,ArrayList<Delivery>> date2deliveries;
+    private  HashMap<Integer,Truck> trucks;
+    private  HashMap<Integer,Delivery> deliveries;
+    private  HashMap<Product,Integer> products;
+    private  HashMap<Integer,Driver> drivers;
+    private  HashMap<LocalDate,ArrayList<Truck>> date2trucks;
+    private  HashMap<LocalDate,ArrayList<Driver>> date2drivers;
+    private  HashMap<LocalDate,ArrayList<Delivery>> date2deliveries;
+    private int deliveryCounter = 0;
 
-private int deliveryCounter = 0;
-
-
-
-public LogisticsCenter( HashMap<Integer,Truck> trucks,HashMap<Integer,Delivery> deliveries,
+    public LogisticsCenter( HashMap<Integer,Truck> trucks,HashMap<Integer,Delivery> deliveries,
                         HashMap<Product,Integer> products,HashMap<Integer,Driver> drivers){
-        
-                this.trucks = trucks;
-                this.deliveries = deliveries;
-                this.products = products;
-                this.drivers = drivers;            
+        this.trucks = trucks;
+        this.deliveries = deliveries;
+        this.products = products;
+        this.drivers = drivers;
+        this.date2trucks = new HashMap<>();
+        this.date2drivers = new HashMap<>();
+        this.date2deliveries = new HashMap<>();
+    }
+
+    public LogisticsCenter(){
+        this.trucks = new HashMap<>();
+        this.deliveries = new HashMap<>();
+        this.products = new HashMap<>();
+        this.drivers = new HashMap<>();
+        this.date2trucks = new HashMap<>();
+        this.date2drivers = new HashMap<>();
+        this.date2deliveries = new HashMap<>();
     }
  
 
