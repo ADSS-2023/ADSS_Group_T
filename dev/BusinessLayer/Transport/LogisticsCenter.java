76158e0c6d22cc07310b2b19193ef94092b1affa
Noam Shurki <shurkin@post.bgu.ac.il>
2023-04-01 22:28:08 +0300

    change coolingLevel params to enum

--- dev/BusinessLayer/Transport/LogisticsCenter.java
+++ dev/BusinessLayer/Transport/LogisticsCenter.java
@@ -234,10 +234,10 @@ public class LogisticsCenter {
     }
 
     public boolean addTruck(int licenseNumber, String model, int weight, int maxWeight ,
-                         LicenseType licenseType, String coolingLevel){
+                         LicenseType licenseType, CoolingLevel coolingLevel){
         if(trucks.containsKey(licenseNumber))
             return false;
-        trucks.put(licenseNumber,new Truck(licenseNumber,model,weight,maxWeight,licenseType,CoolingLevel.valueOf(coolingLevel)));
+        trucks.put(licenseNumber,new Truck(licenseNumber,model,weight,maxWeight,licenseType,coolingLevel));
         return true;
     }
 
@@ -248,10 +248,10 @@ public class LogisticsCenter {
         return true;
     }
 
-    public boolean addDriver(int id, String name, LicenseType licenseType, String coolingLevel){
+    public boolean addDriver(int id, String name, LicenseType licenseType, CoolingLevel coolingLevel){
         if(drivers.containsKey(id))
             return false;
-        drivers.put(id, new Driver(id, name, licenseType, CoolingLevel.valueOf(coolingLevel)));
+        drivers.put(id, new Driver(id, name, licenseType, coolingLevel));
         return true;
     }
 
