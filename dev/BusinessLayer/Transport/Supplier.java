a5d426d19a70826c8da84ef71c9a38f31ceca50e
Noam Shurki <shurkin@post.bgu.ac.il>
2023-04-01 20:06:58 +0300

    coolingLevel getter added to supplier class

--- dev/BusinessLayer/Transport/Supplier.java
+++ dev/BusinessLayer/Transport/Supplier.java
@@ -4,9 +4,11 @@ public class Supplier extends Site{
 
     CoolingLevel coolingLevel;
     public Supplier(String address,String telNumber,String contactName, CoolingLevel coolingLevel){
-        this.address = address;
-        this.telNumber = telNumber;
-        this.contactName = contactName;
+        super(address,telNumber,contactName);
         this.coolingLevel = coolingLevel;
+    }
+
+    public CoolingLevel getCoolingLevel() {
+        return coolingLevel;
     }
 }
