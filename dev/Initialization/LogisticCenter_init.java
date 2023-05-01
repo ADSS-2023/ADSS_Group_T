185caf418faa6ed2e03e64a3803c3ea7921add95
noamgilad <noamgilad98@gmail.com>
2023-05-01 18:57:41 +0300

    remove truck service and controller

--- dev/Initialization/LogisticCenter_init.java
+++ dev/Initialization/LogisticCenter_init.java
@@ -1,4 +1,13 @@
 package Initialization;
 
+import ServiceLayer.Transport.LogisticCenterService;
+
 public class LogisticCenter_init {
+    public static void init(LogisticCenterService logisticCenterService){
+        logisticCenterService.addTruck(2001, "Truck1", 4000 , 8000, 3);
+        logisticCenterService.addTruck(2002, "Truck2", 8000, 13000, 2);
+        logisticCenterService.addTruck(2003, "Truck3", 12500, 20000, 1);
+        logisticCenterService.addTruck(2004, "Truck4", 15000, 22000, 3);
+        logisticCenterService.addTruck(2005, "Truck5", 20000, 30000, 1);
+    }
 }
