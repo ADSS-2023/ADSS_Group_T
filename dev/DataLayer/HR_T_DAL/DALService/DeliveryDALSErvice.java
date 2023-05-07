4b25bcc7977fc48b07f3cfde7674b5fbc0ae79d1
Israel Vaitzman <israelw@post.bgu.ac.il>
2023-05-07 18:57:32 +0300

    DAL services

---
+++ dev/DataLayer/HR_T_DAL/DALService/DeliveryDALSErvice.java
@@ -0,0 +1,4 @@
+package DataLayer.HR_T_DAL.DALService;
+
+public class DeliveryDALSErvice {
+}
---
+++ dev/DataLayer/HR_T_DAL/DALService/LogisticCenterDALService.java
@@ -0,0 +1,8 @@
+package DataLayer.HR_T_DAL.DALService;
+
+public class LogisticCenterDALService {
+    public LogisticCenterDALService(){
+
+    }
+}
+
