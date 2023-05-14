b28f05a9b45fc5f79205d46e8e109c0eccf95b73
noamgilad <noamgilad98@gmail.com>
2023-05-14 16:15:18 +0300

    LogisticCenterControllerTest

--- dev/Tests/BusinessLayer/Transport/BranchControllerTest.java
+++ dev/Tests/BusinessLayer/Transport/BranchControllerTest.java
@@ -3,16 +3,22 @@ package BusinessLayer.Transport;
 import DataLayer.HR_T_DAL.DalService.DalDeliveryService;
 import DataLayer.HR_T_DAL.DalService.DalLogisticCenterService;
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class BranchControllerTest extends TestCase {
     private BranchController branchController;
     private DalDeliveryService dalDeliveryService;
 
     private DalLogisticCenterService dalLogisticCenterService;
+
+    @Before
     public void setUp() throws Exception {
         super.setUp();
         String testDBUrl = "jdbc:sqlite:dev/DataLayer/HR_Transport_DB.db";
@@ -23,7 +29,7 @@ public class BranchControllerTest extends TestCase {
     }
 
     public void tearDown() throws Exception {
-       // dalDeliveryService.deleteAllData();
+       dalDeliveryService.deleteAllData();
     }
 
     public void testAddBranch() throws SQLException {
@@ -57,38 +63,28 @@ public class BranchControllerTest extends TestCase {
         Branch result = branchController.getBranch(branchAddress);
 
         // check if the returned branch is correct
-        assertEquals(branch, result);
+        assertEquals(branch.address, result.address);
     }
 
 
-    public void testGetAllBranches() throws SQLException {
-//        // create test data
-//        String branchAddress1 = "Test Branch Address 1";
-//        String telNumber1 = "123456789";
-//        String contactName1 = "Test Contact Name 1";
-//        int x1 = 1;
-//        int y1 = 2;
-//
-//        String branchAddress2 = "Test Branch Address 2";
-//        String telNumber2 = "987654321";
-//        String contactName2 = "Test Contact Name 2";
-//        int x2 = 3;
-//        int y2 = 4;
-//
-//        // add branches to the system
-//        Branch branch1 = new Branch(branchAddress1, telNumber1, contactName1, x1, y1);
-//        dalDeliveryService.insertBranch(branch1);
-//        branches.put(branchAddress1, branch1);
-//
-//        Branch branch2 = new Branch(branchAddress2, telNumber2, contactName2, x2, y2);
-//        dalDeliveryService.insertBranch(branch2);
-//        branches.put(branchAddress2, branch2);
-//
-//        // call the method under test
-//        LinkedHashMap<String, Branch> result = branchController.getAllBranches();
-//
-//        // check if the returned map is correct
-//        assertEquals(branches, result);
+    @Test
+    public void testAddBranchWithDuplicateAddress() throws SQLException {
+        // try to add the second branch with the same address
+        try {
+            // create test data
+            String branchAddress = "Test Branch Address";
+            String telNumber = "123456789";
+            String contactName = "Test Contact Name";
+            int x = 1;
+            int y = 2;
+
+            // add the first branch
+            branchController.addBranch(branchAddress, telNumber, contactName, x, y);
+            branchController.addBranch(branchAddress, telNumber, contactName, x, y);
+            fail("Expected an SQLException to be thrown");
+        } catch (Exception e) {
+            assertEquals("branch address is taken", e.getMessage());
+        }
     }
 
 
