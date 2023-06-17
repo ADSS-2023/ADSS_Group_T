dd041f1e17fcf9a1bdb9f1921304893e7658d7e7
noamgilad <noamgilad98@gmail.com>
2023-06-17 10:00:33 +0300

    fix supp products

--- dev/DataLayer/HR_T_DAL/DB_init/SiteAddresses.java
+++ dev/DataLayer/HR_T_DAL/DB_init/SiteAddresses.java
@@ -37,9 +37,9 @@ public class SiteAddresses {
             case 2:
                 return "Fish Market, Tel Aviv-Yafo";
             case 3:
-                return "Happy Market, Efrayim Kishon St 9, Be'er Ya'akov";
+                return "Happy Market, Efrayim Kishon St 9, Beer Yaakov";
             case 4:
-                return "Vegetables store, HaMa'avak St 43, Giv'atayim";
+                return "Vegetables store, HaMaavak St 43, Givatayim";
             case 5:
                 return "Bana drinks, Ha-Manof St 8, Rehovot";
             case 6:
